b1243b2eddd4cc836b5e1ea42a85d1a9b94b55cd
nkk36 <nkallfa36@gmail.com>
2017-08-24 22:10:39 -0400

    Commented out code in GetBreweryBeerTable.py that was unnecessary

--- GetBreweryBeerTable.py
+++ GetBreweryBeerTable.py
@@ -54,43 +54,25 @@ def GetBreweryBeerTable(links, numbeer):
     
     return(df)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #Get number of breweries
-    numRegex = re.compile(r"\d{1,4}")
-    numactive = int(numRegex.search(soup.find_all("a", href = "#active")[0].text).group())
-    
-    BreweryLinks = soup.find_all("a", href = True)
-    
-    linkRegex = re.compile(r"/brewers/([\D])+(/)([0-9])+")
-     
-    links = []
-    for link in BreweryLinks:
-        if linkRegex.search(str(link)) is not None:
-            links.append(linkRegex.search(str(link)).group())
-        else:
-            pass
-    
-    tlinks = []
-    for i in range(0,numactive,1):
-        tlinks.append("https://www.ratebeer.com/" + links[i])
-    
-    tlinks = pd.DataFrame(tlinks, columns = ["links"])
-    return(tlinks)
-        
+#    #Get number of breweries
+#    numRegex = re.compile(r"\d{1,4}")
+#    numactive = int(numRegex.search(soup.find_all("a", href = "#active")[0].text).group())
+#    
+#    BreweryLinks = soup.find_all("a", href = True)
+#    
+#    linkRegex = re.compile(r"/brewers/([\D])+(/)([0-9])+")
+#     
+#    links = []
+#    for link in BreweryLinks:
+#        if linkRegex.search(str(link)) is not None:
+#            links.append(linkRegex.search(str(link)).group())
+#        else:
+#            pass
+#    
+#    tlinks = []
+#    for i in range(0,numactive,1):
+#        tlinks.append("https://www.ratebeer.com/" + links[i])
+#    
+#    tlinks = pd.DataFrame(tlinks, columns = ["links"])
+#    return(tlinks)
+#        
